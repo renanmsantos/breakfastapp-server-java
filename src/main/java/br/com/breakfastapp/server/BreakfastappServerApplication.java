c7ce68e5f0c98fee8adc7e431885d8cf06fa2375
Renan Moreira Santos <renanmoreirasan@gmail.com>
2019-06-18 21:23:12 -0300

    First commit

---
+++ src/main/java/br/com/breakfastapp/server/BreakfastappServerApplication.java
@@ -0,0 +1,13 @@
+package br.com.breakfastapp.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BreakfastappServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BreakfastappServerApplication.class, args);
+	}
+
+}
