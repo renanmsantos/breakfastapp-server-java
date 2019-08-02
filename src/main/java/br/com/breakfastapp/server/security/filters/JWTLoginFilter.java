ab4f470adab866661af687e9021bddf9c1d34b4f
Renan Moreira Santos <renanmoreirasan@gmail.com>
2019-08-01 23:53:48 -0300

    Updated username to e-mail at authentication.

--- src/main/java/br/com/breakfastapp/server/security/filters/JWTLoginFilter.java
+++ src/main/java/br/com/breakfastapp/server/security/filters/JWTLoginFilter.java
@@ -32,7 +32,7 @@ public class JWTLoginFilter extends AbstractAuthenticationProcessingFilter {
 
         return getAuthenticationManager().authenticate(
                 new UsernamePasswordAuthenticationToken(
-                        credentials.getUsername(),
+                        credentials.getEmail(),
                         credentials.getPassword()
                 )
         );
